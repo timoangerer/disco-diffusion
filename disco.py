f7847761390ce1311cdb36dee6052773be866baa
timoangerer <timo.angerer@code.berlin>
2022-04-14 15:43:36 +0000

    Catch nvidia-smi error if not available

--- disco.py
+++ disco.py
@@ -354,18 +354,22 @@ def main(cfg):
   # !!    "id": "CheckGPU"
   # !! }}
   #@title 1.1 Check GPU Status
-  import subprocess
-  simple_nvidia_smi_display = cfg.simple_nvidia_smi_display#@param {type:"boolean"}
-  if simple_nvidia_smi_display:
-    #!nvidia-smi
-    nvidiasmi_output = subprocess.run(['nvidia-smi', '-L'], stdout=subprocess.PIPE).stdout.decode('utf-8')
-    print(nvidiasmi_output)
-  else:
-    #!nvidia-smi -i 0 -e 0
-    nvidiasmi_output = subprocess.run(['nvidia-smi'], stdout=subprocess.PIPE).stdout.decode('utf-8')
-    print(nvidiasmi_output)
-    nvidiasmi_ecc_note = subprocess.run(['nvidia-smi', '-i', '0', '-e', '0'], stdout=subprocess.PIPE).stdout.decode('utf-8')
-    print(nvidiasmi_ecc_note)
+  try:
+    import subprocess
+    simple_nvidia_smi_display = cfg.simple_nvidia_smi_display#@param {type:"boolean"}
+    if simple_nvidia_smi_display:
+      #!nvidia-smi
+      nvidiasmi_output = subprocess.run(['nvidia-smi', '-L'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+      print(nvidiasmi_output)
+    else:
+      #!nvidia-smi -i 0 -e 0
+      nvidiasmi_output = subprocess.run(['nvidia-smi'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+      print(nvidiasmi_output)
+      nvidiasmi_ecc_note = subprocess.run(['nvidia-smi', '-i', '0', '-e', '0'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+      print(nvidiasmi_ecc_note)
+  except:
+    print("Could not use nvidia-smi command.")
+
 
   # %%
   # !! {"metadata": {
