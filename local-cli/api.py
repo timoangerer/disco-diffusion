dd452ce57bcc46e20cc84ebf29dc32b8bcdc4fa4
timoangerer <timo.angerer@code.berlin>
2022-05-04 12:43:30 +0000

    Submit jobs to aws batch via CLI

--- local-cli/api.py
+++ local-cli/api.py
@@ -1,3 +1,44 @@
+from operator import concat
+import sys
+import os
+import re
+import json
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(SCRIPT_DIR))
+
+import boto3
+
+client = boto3.client('batch')
+import uuid
+
 def create_job(job_dto: dict):
-    print("creating job: ", job_dto)
-    pass
+    job_dto["batch_name"] = f"{job_dto['batch_name']}-{uuid.uuid4()}"
+
+    print("job_dto: ", job_dto)
+    
+    send_aws_batch_job(job_dto)
+
+def send_aws_batch_job(job_dto):
+    container_command = generate_container_cmd(job_dto)
+
+    tags = {str(k): "".join(re.findall("[a-zA-Z0-9_/+\-\=. ]*", str(v))) for (k,v) in job_dto.items()}
+    print("Tags: ", tags)
+
+    response = client.submit_job(
+        jobName=job_dto["batch_name"],
+        jobQueue='job-queue-dd1',
+        jobDefinition='job-definition-dd1',
+        containerOverrides={
+            'command': container_command
+        },
+        propagateTags=False,
+        tags=tags
+    )
+
+    print("Submit job response: ", response)
+
+def generate_container_cmd(paramters):
+    params_list = [f'{k}={json.dumps(v)}' for (k,v) in paramters.items()]
+    params_list = concat(["python", "disco.py"], params_list)
+    return params_list
--- local-cli/gen_cli.py
+++ local-cli/gen_cli.py
@@ -1,12 +1,19 @@
 from omegaconf import DictConfig, OmegaConf
 from hydra.core.hydra_config import HydraConfig
 import hydra
+import re
+import api as API
 
 @hydra.main(config_path="../config", config_name="defaults")
 def gen_cli(cfg: DictConfig) -> None:
-    cfg = OmegaConf.to_container(cfg, resolve=True) # resolve all interpolations eagerly
-    print(OmegaConf.to_yaml(cfg))
-    print(HydraConfig.get().overrides.task)
+    overrides = list(HydraConfig.get().overrides.task)
+    overrides = list(filter(lambda it: not bool(re.search(r"(sweep)", it)), overrides)) # remove sweep variables
+    overrides_cfg = OmegaConf.from_cli(overrides)
+    overrides_cfg["batch_name"] = cfg["batch_name"]
+    print(OmegaConf.to_yaml(overrides_cfg))
+
+    API.create_job(OmegaConf.to_container(overrides_cfg))
+
 
 if __name__ == "__main__":
     gen_cli()
