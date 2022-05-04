ff86e93e00c8ea0fc459d9d3f6e3e1b8e42ed4e1
timoangerer <timo.angerer@code.berlin>
2022-05-04 13:23:25 +0000

    Add preview cmd option

--- local-cli/gen_cli.py
+++ local-cli/gen_cli.py
@@ -10,7 +10,13 @@ def gen_cli(cfg: DictConfig) -> None:
     overrides = list(filter(lambda it: not bool(re.search(r"(sweep)", it)), overrides)) # remove sweep variables
     overrides_cfg = OmegaConf.from_cli(overrides)
     overrides_cfg["batch_name"] = cfg["batch_name"]
+
     print(OmegaConf.to_yaml(overrides_cfg))
+    
+    is_preview = "+preview" in overrides_cfg
+    if is_preview:
+        print("(is preview)")
+        return
 
     API.create_job(OmegaConf.to_container(overrides_cfg))
 
